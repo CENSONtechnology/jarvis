18187e52ff5bdcde8888c22300c9ae68c5dbb373
Houpert <xxdiagammaxx@yahoo.fr>
2018-02-27 17:46:39 +0100

    Remvoe specific platform

--- plugins_installed/jarvis-face/install.sh
+++ plugins_installed/jarvis-face/install.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 # Use only if you need to perform changes on the user system such as installing software
 jv_install gifsicle # for gif duration on all platforms (incl. OSX)
-if [ "$platform" == "linux" ]; then
-    jv_install unclutter # hide mouse pointer
-	sudo apt-get install gifsicle
-fi
 
 #Jarvis face module
 sudo apt-get install unclutter
+sudo apt-get install gifsicle
